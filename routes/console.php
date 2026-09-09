1977acb662732c9639af6f6b0d9e53b59a193d78
Levi Zitting <me@levizitting.com>
2026-09-08 22:16:39 -0500

    fix: use typed scheduler output configuration

--- routes/console.php
+++ routes/console.php
@@ -2,9 +2,10 @@
 
 declare(strict_types=1);
 
+use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Schedule;
 
 Schedule::command('neon:poll-participants --quiet')
     ->everyMinute()
     ->withoutOverlapping()
-    ->appendOutputTo(config('logging.scheduler_output'));
+    ->appendOutputTo(Config::string('logging.scheduler_output'));
