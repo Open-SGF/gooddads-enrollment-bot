7ed55919bafba444c1151175bada46ca502222bd
Levi Zitting <me@levizitting.com>
2026-09-08 22:12:13 -0500

    fix: keep scheduler output from corrupting JSON logs

--- routes/console.php
+++ routes/console.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::command('neon:poll-participants')
+Schedule::command('neon:poll-participants --quiet')
     ->everyMinute()
-    ->withoutOverlapping();
+    ->withoutOverlapping()
+    ->appendOutputTo(config('logging.scheduler_output'));
